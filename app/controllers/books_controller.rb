52423c42f0f0328699bac08398a1460feb386291
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-18 13:40:44 +0000

    [Add]いいね機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
-
+  before_action :ensure_correct_user, only: [:edit]
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -32,15 +34,21 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to books_path unless @user == current_user
+  end
+
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
